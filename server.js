5ca98e3df96f1a803c706b75355fdb7eefc53f7f
Lucy <mayukuchler@gmail.com>
2018-10-15 18:52:05 -0400

    final1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require("path");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
